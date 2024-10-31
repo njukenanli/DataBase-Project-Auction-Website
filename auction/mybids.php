997009fb37990711bdd176ac0afa13165305e7af
AnqiLiu1 <ucabal9@ucl.ac.uk>
2024-10-31 11:26:15 +0000

    Update mybids.php

--- auction/mybids.php
+++ auction/mybids.php
@@ -6,6 +6,25 @@
 <h2 class="my-3">My bids</h2>
 
 <?php
+// Demonstration of what listings will look like using dummy data.
+  $item_id = "87021";
+  $title = "Dummy title";
+  $description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget rutrum ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus feugiat, ipsum vel egestas elementum, sem mi vestibulum eros, et facilisis dui nisi eget metus. In non elit felis. Ut lacus sem, pulvinar ultricies pretium sed, viverra ac sapien. Vivamus condimentum aliquam rutrum. Phasellus iaculis faucibus pellentesque. Sed sem urna, maximus vitae cursus id, malesuada nec lectus. Vestibulum scelerisque vulputate elit ut laoreet. Praesent vitae orci sed metus varius posuere sagittis non mi.";
+  $current_price = 30;
+  $num_bids = 1;
+  $end_date = new DateTime('2020-09-16T11:00:00');
+  
+  // This uses a function defined in utilities.php
+  print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
+  
+  $item_id = "516";
+  $title = "Different title";
+  $description = "Very short description.";
+  $current_price = 13.50;
+  $num_bids = 3;
+  $end_date = new DateTime('2020-11-02T00:00:00');
+  
+  print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
   // This page is for showing a user the auctions they've bid on.
   // It will be pretty similar to browse.php, except there is no search bar.
   // This can be started after browse.php is working with a database.
