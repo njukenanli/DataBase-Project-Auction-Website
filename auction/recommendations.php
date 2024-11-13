bc2e4e9c4fc601e8a1b246cf0a5068042bcc27de
AnqiLiu1 <ucabal9@ucl.ac.uk>
2024-11-13 22:54:24 +0000

    Update recommendations.php

--- auction/recommendations.php
+++ auction/recommendations.php
@@ -12,11 +12,57 @@
   // Feel free to extract out useful functions from browse.php and put them in
   // the shared "utilities.php" where they can be shared by multiple files.
   
-  
-  // TODO: Check user's credentials (cookie/session).
-  
+  // DONE: Check user's credentials (cookie/session).
+	session_start();
+	if (!isset($_SESSION['logged_in']) || $_SESSION['account_type'] != 'buyer'){
+	header("Location: browse.php");
+	exit();
+	}
+	
+	//get user's ID
+	$user_id = $_SESSION['user_id'];
+	
+	//connect with the database
+	$conn = ConnectDB();
+
   // TODO: Perform a query to pull up auctions they might be interested in.
-  
-  // TODO: Loop through results and print them out as list items.
-  
+	$sql = "SELECT Item.item_ID, Category.name AS title, Item.description, bid.bid_price, COUNT(Bid.bid_ID) AS num_bids, Item.end_date
+		FROM Bid, Item, Category, Buyer
+		WHERE Bid.item_ID IN (SELECT item_ID FROM Bid WHERE buyer_ID = ?) AND
+      			buyer_id != ? AND
+			Buyer.user_ID = Bid.buyer_ID AND
+			Bid.item_ID = Item.item_ID AND
+			Item.category_ID = Category.category_ID
+		GROUP BY Item.item_ID
+		ORDER BY bid_time DESC";
+	if($stmt = $conn->prepare($sql)){
+		$stmt->bind_param("i", $user_id);
+		$stmt->execute();
+
+		//get results
+		$result = $stmt->get_result();
+		if($result->num_rows > 0){
+			echo "<ul class = 'list-group'>";
+
+			//print out all the results as list items
+			while($row = $result -> fetch_assoc()){
+				$item_id = $row['item_ID'];
+				$title = $row['title'];
+                    		$desc = $row['description'];
+                    		$price = $row['bid_price'];
+                    		$num_bids = $row['num_bids'];
+                    		$end_time = $row['end_date'];
+				print_listing_li($item_id, $title, $desc, $price, $num_bids, $end_time);
+			}
+			echo "</ul>";
+		} else {
+			echo "<p> More exciting to come... </p>";
+		}
+		//$stmt -> close();
+	} else {
+		echo "<p>Error querying the database.</p>";
+	}
+	$conn->close();
 ?>
+</div>
+<?php include_once("footer.php")?>
