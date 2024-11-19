e767e1e68ae52a9b77449cd31846886feeb38573
AnqiLiu1 <ucabal9@ucl.ac.uk>
2024-11-19 00:23:01 +0000

    Update recommendations.php
    Delete the reduplicative items. Now buyer can only see the recommended items that they did not bid on.

--- auction/recommendations.php
+++ auction/recommendations.php
@@ -12,64 +12,92 @@
   // Feel free to extract out useful functions from browse.php and put them in
   // the shared "utilities.php" where they can be shared by multiple files.
   
-  // DONE: Check user's credentials (cookie/session).
-	if (!isset($_SESSION['logged_in']) || $_SESSION['account_type'] != 'buyer'){
-	header("Location: browse.php");
-	exit();
-	}
+// DONE: Check user's credentials (cookie/session).
+if (!isset($_SESSION['logged_in']) || $_SESSION['account_type'] != 'buyer'){
+header("Location: browse.php");
+exit();
+}
 	
-	//get user's email
-	$user_email = $_SESSION['username'];
-	
-	//connect with the database
-	$conn = ConnectDB();
-  // DONE: Perform a query to pull up auctions they might be interested in.
-	$sql = "SELECT Item.item_ID,
-			Category.name AS title,
-			Item.description,
-			(SELECT COUNT(bid_ID) FROM Bid WHERE Bid.item_ID = Item.item_ID) AS num_bids,
-			Item.end_date,
-			(SELECT GREATEST(
-				(SELECT starting_price FROM Item WHERE Bid.item_ID = Item.item_ID),
-				(SELECT COALESCE(MAX(bid_price), 0) FROM Bid WHERE Bid.item_ID = Item.item_ID)
-			)) AS bid_price
-		FROM Bid, Item, Category, Buyer
-		WHERE Bid.item_ID IN (SELECT item_ID FROM Bid, Buyer WHERE Buyer.user_ID = Bid.buyer_ID AND Buyer.email = ?) AND
-			Buyer.email != ? AND
-			Buyer.user_ID = Bid.buyer_ID AND
-			Bid.item_ID = Item.item_ID AND
-			Item.category_ID = Category.category_ID
-		GROUP BY Item.item_ID, Category.name, Item.description, Item.end_date
-		ORDER BY Item.end_date DESC;
-  		";
-	if($stmt = $conn->prepare($sql)){
-		$stmt->bind_param("ss", $user_email, $user_email);
-		$stmt->execute();
+//connect with the database
+$conn = ConnectDB();
 
-		//get results
-		$result = $stmt->get_result();
-		if($result->num_rows > 0){
-			echo "<ul class = 'list-group'>";
+//get user's id
+$user_email = $_SESSION['username'];
+$sql = "SELECT user_ID FROM Buyer WHERE email = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $user_email);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+$user_id = $row['user_ID'];
 
-			//DONE: print out all the results as list items
-			while($row = $result -> fetch_assoc()){
-				$item_id = $row['item_ID'];
-				$title = $row['title'];
-                    		$desc = $row['description'];
-                    		$price = $row['bid_price'];
-                    		$num_bids = $row['num_bids'];
-                    		$end_time = new DateTime($row['end_date']);
-				print_listing_li($item_id, $title, $desc, $price, $num_bids, $end_time);
-			}
-			echo "</ul>";
-		} else {
-			echo "<p> More exciting to come... </p>";
+// DONE: Perform a query to pull up auctions they might be interested in.
+//Create a temporary table to get other buyers have bidded the same item with the current buyer
+$sql = "CREATE TEMPORARY TABLE otherBuyers AS
+	SELECT b2.buyer_ID
+	FROM Bid b1, Bid b2
+	WHERE b1.item_ID = b2.item_ID 	AND
+    		b1.buyer_ID = ?		AND
+    		b2.buyer_ID != ?
+	GROUP BY b2.buyer_ID";
+
+//pre-processing the query statements
+if ($stmt = $conn->prepare($sql)) {
+    	$stmt->bind_param("ii", $user_id, $user_id);
+    	$stmt->execute();
+	$stmt->close();
+} else {
+	die ("Error creating temporary table.");
+}
+//Get recommended items from other buyers who have similar interest
+//Excepting bidden items
+$sql = "SELECT Item.item_ID,
+		Item.title,
+		Item.description,
+		Item.end_date,
+		(SELECT COUNT(bid_ID) FROM Bid WHERE Bid.item_ID = Item.item_ID) AS num_bids,
+		(SELECT GREATEST(
+			(SELECT starting_price FROM Item WHERE Bid.item_ID = Item.item_ID),
+			(SELECT COALESCE(MAX(bid_price), 0) FROM Bid WHERE Bid.item_ID = Item.item_ID)
+		)) AS bid_price
+	FROM Bid, otherBuyers, Item
+	WHERE Bid.buyer_ID = otherBuyers.buyer_ID AND
+		Bid.item_ID = Item.item_ID AND
+  		Item.item_ID NOT IN (SELECT item_ID FROM Bid WHERE buyer_ID = ?)";
+
+//pre-processing query statement
+if($stmt = $conn->prepare($sql)){
+	$stmt->bind_param("i", $user_id);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	//Print out all the recommended items, if there is
+	if($result->num_rows > 0){
+		echo "<ul class = 'list-group'>";
+		
+		while($row = $result -> fetch_assoc()){
+			$item_id = $row['item_ID'];
+			$title = $row['title'];
+            		$desc = $row['description'];
+                    	$price = $row['bid_price'];
+                    	$num_bids = $row['num_bids'];
+        		$end_time = new DateTime($row['end_date']);
+
+			//Call print list function
+			print_listing_li($item_id, $title, $desc, $price, $num_bids, $end_time);
 		}
-		$stmt -> close();
+		
+		echo "</ul>";
 	} else {
-		echo "<p>Error querying the database.</p>";
+		//If there is no recommened items, giving some hints
+		echo "<p> More exciting to come... </p>";
 	}
-	$conn->close();
+	$stmt -> close();
+} else {
+	//Give some hints when there is query error.
+	echo "<p>Error querying the database.</p>";
+}
+$conn->close();
 ?>
 </div>
+
 <?php include_once("footer.php")?>
